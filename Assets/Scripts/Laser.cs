662ef64f40efe6d6f96cc7cfcdab4567691e03c4
Hugi Freyr Alfgeirsson <hugi22@ru.is>
2024-03-06 14:51:39 +0000

    New slime sound imports slime scene 2 updates

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -73,7 +73,7 @@ public class Laser : MonoBehaviour
             soundPlayed = true;
         }
 
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(1.5f);
 
         player.transform.position = respawnLocation;
 
